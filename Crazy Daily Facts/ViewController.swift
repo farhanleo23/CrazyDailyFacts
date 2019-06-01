7b71becedca4f7335cf9145197830785041e0e5a
farhan hussain <farhanleo23@gmail.com>
2019-06-02 00:38:55 +0200

    major update

--- Crazy Daily Facts/ViewController.swift
+++ Crazy Daily Facts/ViewController.swift
@@ -9,19 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-
+    
     @IBOutlet weak var funFactLabel: UILabel!
+    
+    let factProvider = FactProvider()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        funFactLabel.text = factProvider.randomFact()
     }
 
     @IBAction func showFact() {
-        funFactLabel.textColor = UIColor.red
-        funFactLabel.text = "SMITHA WAKE UP"
+        funFactLabel.text = factProvider.randomFact()
     }
     
 }
